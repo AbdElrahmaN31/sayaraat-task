6c3172fc58ed16a451d6d735e072b019bdc6c090
Abdulrahman Nashaat <abdelrahmanndev@gmail.com>
2024-04-27 22:26:32 +0300

    initial commit

---
+++ app/Helpers/Helpers.php
@@ -0,0 +1,55 @@
+<?php
+
+if (!function_exists('lang')) {
+    function lang(): string
+    {
+        return App()->getLocale();
+    }
+}
+
+if (!function_exists('generateRandomCode')) {
+    function generateRandomCode(): string
+    {
+        return '1234';
+        return rand(1111, 4444);
+    }
+}
+
+if (!function_exists('randomBy')) {
+    function randomBy($num = 6): int
+    {
+        $start = (int)str_repeat(1, $num);
+        $end   = (int)str_repeat(9, $num);
+        return rand($start, $end);
+    }
+}
+
+/**
+ * round number to 2 precision
+ */
+if (!function_exists('round2')) {
+    function round2($number): float
+    {
+        return round($number, 2);
+    }
+}
+
+if (!function_exists('round1')) {
+    function round1($number): float
+    {
+        return round($number, 1);
+    }
+}
+
+if (!function_exists('perPage')) {
+    function perPage($perPage = 10)
+    {
+        if (request('per_page') == 'all') {
+            return 999;
+        }
+
+        return request('per_page') ? (int)request('per_page') : $perPage;
+    }
+}
+
+?>
